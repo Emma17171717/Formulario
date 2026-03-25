34bd0031d81bf3aaf781636f0cc0d969957186ad
Emma17171717 <126958679+Emma17171717@users.noreply.github.com>
2026-03-25 16:04:11 -0500

    Delete principal.py

--- principal.py
+++
@@ -1,9 +0,0 @@
-from formulario import Formulario
-
-#zona codigo principal 
-obj_formulario = Formulario()
-
-#linea 9 es traer la ventana....
-aux_formulario = obj_formulario.iniciar_ventana()
-obj_formulario.iniciar_preguntas()
-aux_formulario.mainloop()

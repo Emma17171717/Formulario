3ff7b2980bf57c1cd3dd428316a5ece19964bca6
Emma17171717 <126958679+Emma17171717@users.noreply.github.com>
2026-04-15 11:52:44 -0500

    Update formulario.py

--- Ventana/formulario.py
+++ Ventana/formulario.py
@@ -6,6 +6,7 @@ class Formulario:
         self.entry_nombre = " "
         self.label_resultado = " "
         self.formulario = " "
+        self.datos_cliente = []
 
     def iniciar_ventana(self):
         self.formulario = Ventana.Tk() #widgets
@@ -25,10 +26,71 @@ class Formulario:
         self.entry_nombre.config(bg = "yellow")
         self.entry_nombre.pack()
 
+        label_apellido = Ventana.Label(text = "Digite el apellido:")
+        label_apellido.pack()
+
+        self.entry_apellido = Ventana.Entry(self.formulario)
+        self.entry_apellido.pack()
+
+        label_cedula = Ventana.Label(text = "Digite la cedula:")
+        label_cedula.pack()
+
+        self.entry_cedula = Ventana.Entry(self.formulario)
+        self.entry_cedula.pack()
+
+
+        label_telefono = Ventana.Label(text = "Digite el telefono:")
+        label_telefono.pack()
+
+        self.entry_telefono = Ventana.Entry(self.formulario)
+        self.entry_telefono.pack()
+
+
+        label_correo = Ventana.Label(text = "Digite el correo:")
+        label_correo.pack()
+
+        self.entry_correo = Ventana.Entry(self.formulario)
+        self.entry_correo.pack()
+
         #button ----> boton
         boton_enviar = Ventana.Button(self.formulario, text="Enviar", command=self.hacer_clic) #para enviar informacion, usar Lambda
         boton_enviar.configure(bg="orange")
-        boton_enviar.pack()  
+        boton_enviar.pack()
 
-    def hacer_clic(self):
-        print("clik....")
+    self.label_resultado = Ventana.Label(self.formulario,text="")
+        self.label_resultado.pack()
+
+def validar_campos(self):
+        if self.entry_nombre.get() == "":
+            return False
+        if self.entry_apellido.get() == "":
+            return False
+        if self.entry_cedula.get() == "":
+            return False
+        if self.entry_telefono.get() == "":
+            return False
+        if self.entry_correo.get() == "":
+            return False
+        return True
+
+    def guardar_datos(self):
+        self.datos_cliente = [
+            self.entry_nombre.get(),
+            self.entry_apellido.get(),
+            self.entry_cedula.get(),
+            self.entry_telefono.get(),
+            self.entry_correo.get()
+        ]
+
+    def mostrar_resultado(self):
+        self.label_resultado.config(text="Formulario guardado correctamente")
+        print("Datos guardados:")
+        print(self.datos_cliente)
+
+    def procesar_formulario(self):
+        if self.validar_campos():
+            self.guardar_datos()
+            self.mostrar_resultado()
+
+        else:
+            self.label_resultado.config(text="Hay campos vacios")
